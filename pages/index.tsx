4fd63fd3f0f335dcd1c90111a8de632bce1e4d90
bastien-lch <74551941+bastien-lch@users.noreply.github.com>
2023-06-06 09:47:47 +0200

    action ssh

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Bienvenue a <a href="https://nextjs.org">Next.js!</a>
+          hello <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
